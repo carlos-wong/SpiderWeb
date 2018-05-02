fe0ecf02dae0521aa66cad470282f8efe639a781
carlos <huaixian.huang@gmail.com>
2018-05-02 15:43:11 +0800

    Doc: use mogoose

--- server/index.js
+++ server/index.js
@@ -1,7 +1,16 @@
 const Koa = require('koa');
-const app = new Koa();
 var Router = require('koa-router');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://mongo:27017/myproject');
+var db = mongoose.connection;
+
+const app = new Koa();
 var router = new Router();
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log('opended mongodb');
+});
 
 router.get('/', async (ctx, next) => {
     // ctx.router available
