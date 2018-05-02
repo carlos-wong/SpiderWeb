8f8d947021364b58c7636b9322966f6e5205b7c6
carlos <huaixian.huang@gmail.com>
2018-05-02 15:25:29 +0800

    Feat: koa-router work

--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
 const Koa = require('koa');
 const app = new Koa();
+var Router = require('koa-router');
+var router = new Router();
 
-app.use(async ctx => {
+router.get('/', async (ctx, next) => {
+    // ctx.router available
+    await next();
     ctx.body = 'Hello World carlos';
 });
+
+app.use(router.routes())
+    .use(router.allowedMethods());
 
 app.listen(3000);
