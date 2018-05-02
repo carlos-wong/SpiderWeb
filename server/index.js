ca80ff6f15fe2294722b9b969c22b74add8778a1
carlos <huaixian.huang@gmail.com>
2018-05-02 15:58:51 +0800

    Feat: userinfo table

--- server/index.js
+++ server/index.js
@@ -7,9 +7,21 @@ var db = mongoose.connection;
 const app = new Koa();
 var router = new Router();
 
+var UserInfo = mongoose.model('UserInfo', {
+    username:{ type: String,required: true, index: true,unique: true, dropDups: true, sparse: true },
+    password:{ type: String,required: true}
+});
+
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
     console.log('opended mongodb');
+});
+
+router.get('/register', async (ctx, next) => {
+    console.log('debug register ctx:',ctx.request.query);
+    await next();
+    ctx.status = 200;
+    // ctx.body = 'Hello World carlos';
 });
 
 router.get('/', async (ctx, next) => {
