901ba71ccd5f0a1384e09eada9201a998b9555c1
carlos <huaixian.huang@gmail.com>
2018-05-03 16:02:25 +0800

    Feat: get username and password input

--- react-frontend/src/Login.js
+++ react-frontend/src/Login.js
@@ -3,26 +3,54 @@ import {Row, Col,Layout,Modal,Button,Divider, List, Avatar,Input } from 'antd';
 
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading:false,
+            username:"",
+            password:"",
+        };
+    }
     render() {
         return (
             <div className="App">
               <header className="App-header">
                 <h1 className="App-title">Welcome to SpiderWeb</h1>
               </header>
-              <p className="Login">
+              <div className="Login">
                 <div className="Login_Bar">
                   <a className="Login_Bar_title">Account:</a>
-                  <Input className="Login_Bar_input" placeholder="Username" />
+                  <Input className="Login_Bar_input" placeholder="Username"
+                         value={this.state.username}
+                         onChange={(event)=>{
+                             this.setState({
+                                 username:event.target.value
+                             });
+                    }}/>
                 </div>
-              </p>
-              <p className="Login">
+              </div>
+              <div className="Login">
                 <div className="Login_Bar">
                   <a className="Login_Bar_title">Password:</a>
-                  <Input className="Login_Bar_input" placeholder="Password" />
+                  <Input className="Login_Bar_input" placeholder="Password"
+                         value={this.state.password}
+                         onChange={(event)=>{
+                             this.setState({
+                                 password:event.target.value
+                             });
+                             
+                    }}/>
                 </div>
-              </p>
+              </div>
+              <Button type="primary" loading={this.state.loading} onClick={()=>{
+                    this.setState({loading:true});
+                    console.log('debug state:',this.state);
+                }}>
+                Click me!
+              </Button>
             </div>
         );
     }
