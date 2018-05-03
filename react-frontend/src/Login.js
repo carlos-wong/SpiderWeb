d174fe64bf460da3e07671365a0e390f27bddf2b
carlos <huaixian.huang@gmail.com>
2018-05-03 14:20:07 +0800

    Feat: Login component

---
+++ react-frontend/src/Login.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import {Row, Col,Layout,Modal,Button,Divider, List, Avatar,Input } from 'antd';
+
+import logo from './logo.svg';
+import './App.css';
+
+const Login = () => (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Welcome to SpiderWeb</h1>
+      </header>
+      <p className="Login">
+        <div className="Login_Bar">
+          <a className="Login_Bar_title">Account:</a>
+          <Input className="Login_Bar_input" placeholder="Username" />
+        </div>
+      </p>
+      <p className="Login">
+        <div className="Login_Bar">
+          <a className="Login_Bar_title">Password:</a>
+          <Input className="Login_Bar_input" placeholder="Password" />
+        </div>
+      </p>
+    </div>
+);
+export default Login;
