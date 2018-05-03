0d2b09c2eed75d79c2258a669726119808303e30
carlos <huaixian.huang@gmail.com>
2018-05-03 11:13:19 +0800

    Refactor: split react dev env out of docker compose

--- README.md
+++ README.md
@@ -5,3 +5,15 @@
 1. 安装 docker 和 docker-compose
 2. 在根目录执行 docker-compose up
 3. 访问 http://127.0.0.1:3030
+
+## 开发前端
+
+可能 3000 的端口已经被使用，可以修改一下端口
+
+例如: PORT=3030 yarn start
+
+1. cd react-frontend
+2. yarn install
+3. yarn start
+
+
--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Modal,Button,Divider, List, Avatar,Input } from 'antd';
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,11 +8,10 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to SpiderWeb</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <p className="Login">
+          hi carlos 
         </p>
       </div>
     );
